aea3f9431dd948d578163a6cbe52dc28b7f9fa55
sathya70 <sathyaarumugam376@gmail.com>
2026-09-22 21:10:39 +0530

    updated

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -2,11 +2,11 @@ package com.example;
 
 public class App {
     public static void main(String[] args) {
-        Task task = new Task(1, "Prepare for Java interview", false);
+        Task task = new Task(1, "Prepare for git  interview", false);
 
         System.out.println("Task Manager");
         System.out.println("-------------");
-        System.out.println("ID: " + task.getId());
+        System.out.println("id: " + task.getId());
         System.out.println("Task: " + task.getTitle());
         System.out.println("Completed: " + task.isCompleted());
     }
