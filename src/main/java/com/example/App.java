d0d2e59fcebe2c3c9e43c655885e8329ddf1340b
sathya70 <sathyaarumugam376@gmail.com>
2026-09-22 21:22:21 +0530

    java task

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -4,7 +4,7 @@ public class App {
     public static void main(String[] args) {
         Task task = new Task(1, "Prepare for git  interview", false);
 
-        System.out.println("Ttask manager");
+        System.out.println("java task manager");
         System.out.println("-------------");
         System.out.println("id: " + task.getId());
         System.out.println("task: " + task.getTitle());
