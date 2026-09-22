fcfc8ea758d6e1d86b75517ee562c83244024a4a
sathya70 <sathyaarumugam376@gmail.com>
2026-09-22 21:20:29 +0530

    my task updated

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -4,7 +4,7 @@ public class App {
     public static void main(String[] args) {
         Task task = new Task(1, "Prepare for git  interview", false);
 
-        System.out.println("Task Manager");
+        System.out.println("my Task Manager");
         System.out.println("-------------");
         System.out.println("id: " + task.getId());
         System.out.println("Task: " + task.getTitle());
