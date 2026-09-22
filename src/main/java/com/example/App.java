8990144fb6dd294240bd2b43040d431307118af4
sathya70 <sathyaarumugam376@gmail.com>
2026-09-22 21:14:17 +0530

    updated

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -4,10 +4,10 @@ public class App {
     public static void main(String[] args) {
         Task task = new Task(1, "Prepare for git  interview", false);
 
-        System.out.println("Task Manager");
+        System.out.println("Ttask manager");
         System.out.println("-------------");
         System.out.println("id: " + task.getId());
-        System.out.println("Task: " + task.getTitle());
+        System.out.println("task: " + task.getTitle());
         System.out.println("Completed: " + task.isCompleted());
     }
 }
